dc99bbed4f8900ce97691ced78535a8a411a2e68
Manon <manon.cantos@viacesi.fr>
2019-11-30 20:45:40 +0100

    essaie pour ajouter une fonction pour valider une tâche

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,35 @@ document.querySelector("#button-add").addEventListener("click", function () {
     var taskName = prompt("Entrez votre tâche :");
 
 
-    // Créer et ajouter un élément au DOM
+    // Créer et ajouter une tâche au DOM
     var newElt = document.createElement("li"); // Création d'un élément li
+    newElt.classList.add("task"); //Création d'une classe
     newElt.textContent = taskName; // Définition de son contenu textuel
-
+    
     //Création d'un bouton supprimé
     let spanDelete = document.createElement("span");
     spanDelete.classList.add("delete");
     spanDelete.textContent="X";
     document.getElementById("list").appendChild(spanDelete);
 
-    document.getElementById("list").appendChild(newElt); // Insertion du nouvel élément
+    //Création d'une checkbox
+    var checkTask = document.createElement("INPUT");
+    checkTask.setAttribute("type", "checkbox");
+    checkTask.classList.add("checkbox");
+    document.getElementById("list").appendChild(checkTask);
 
-    //Permet de modifier la tâche lorsque l'on clique dessus ou de supprimer la tâche
+    document.getElementById("list").appendChild(newElt); // Insertion de la nouvelle tâche
+    
+    //Valider une tâche ou non
+    var checkBox = document.getElementsByClassName("checkbox");
+    var text = document.getElementsByClassName("task");
+    if (checkBox.checked == true){
+        text.style.visibility = "hidden";
+    } else {
+        text.style.visibility = "visible";
+    }
+    
+    //Permet de modifier la tâche lorsque l'on clique dessus
     newElt.addEventListener("click", function (e) {
         alert("Pour modifier votre tâche, tapez du texte.")
         var taskModif = prompt("Modifiez votre tâche :", e.target.textContent);
@@ -27,5 +43,6 @@ document.querySelector("#button-add").addEventListener("click", function () {
         // Suppression de l'élément
         document.getElementById("list").removeChild(newElt);
         document.getElementById("list").removeChild(spanDelete);
+
     });
 });
