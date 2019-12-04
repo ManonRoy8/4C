411b612c49f2a016aee4194262c962e96274c8d2
ManonRoy8 <56684422+ManonRoy8@users.noreply.github.com>
2019-12-04 19:47:52 +0100

    ajout réorganisation tâches + suppression modif

--- assets/jquery/jquery.js
+++ assets/jquery/jquery.js
@@ -14,22 +14,61 @@ $("#button-add").click(function() {
         //Création d'un bouton supprimé
         var spanDelete = $("<span></span>").text("X");
         spanDelete.addClass("delete");
+        spanDelete.attr('title', "Cliquez pour supprimer votre tâche");
         $("#list").append(spanDelete);
+
+        //Création d'un bouton déplacer à la fin
+        var spanBefore= $("<span></span>").text("F");
+        spanBefore.addClass("before");
+        spanBefore.attr('title', "Cliquez pour déplacer votre tâche au début");
+        $("#list").append(spanBefore);
+
+        //Création d'un bouton déplacer à la fin
+        var spanAfeter = $("<span></span>").text("E");
+        spanAfeter.addClass("after");
+        spanAfeter.attr('title', "Cliquez pour déplacer votre tâche à la fin");
+        $("#list").append(spanAfeter);
 
         //Création d'une checkbox
         var checkTask = $("<input></input>");
         checkTask.attr("type", "checkbox");
         checkTask.addClass("checkbox");
-        $("#list").append(checkTask);
+        $(newElt).prepend(checkTask);
 
         $("#list").append(newElt);
     }
+
+    //Permet d'afficher une infobulle
+
+    $(".delete").mouseover(function(){
+        $(this).attr("Ciquez pour supprimer");
+    })
 
     //Permet de supprimer la tâche lorsque qu'on clique sur la croix rouge
     spanDelete.click(function() {
         $(newElt).remove();
         $(checkTask).remove();
-        $(spanDelete).remove();
+        $(spanBefore).remove();
+        $(spanAfeter).remove();
+        $(spanDelete).remove();        
+    });
+
+    //Permet de déplacer la tâche au début de la liste 
+    spanBefore.click(function() {
+        $("#list").prepend(newElt);  
+        $(newElt).prepend(checkTask);     
+        $("#list").prepend(spanAfeter);
+        $("#list").prepend(spanBefore);
+        $("#list").prepend(spanDelete);
+    });
+
+    //Permet de déplacer la tâche à la fin de la liste 
+    spanAfeter.click(function() {
+        $("#list").append(spanDelete);
+        $("#list").append(spanBefore);
+        $("#list").append(spanAfeter);
+        $(newElt).prepend(checkTask);
+        $("#list").append(newElt);       
     });
 
     //Valider une tâche ou non
